da662527ecdfc08f72915ebd3b6847ccc1f51faf
Wonjin-david <22000286@handong.ac.kr>
2023-04-23 22:34:00 +0900

    update loadData

--- manager.c
+++ manager.c
@@ -57,7 +57,26 @@ int loadData(Product *p){
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
 
+       fp=fopen("product.txt","rt");
+	  char trash;
+	
+        if(fp==NULL){
+                printf("=> 파일 없음!\n");
+                return 0;
+        }
 
+        //파일 내용을 읽어와서 배열에 값 추가하기
+        for(count=0;count<100;count++){
+                fscanf(fp,"%[^,]s",p[count].name);
+                if(feof(fp)) break;
+                fscanf(fp,"%c",&trash);
+                fscanf(fp,"%d",&p[count].weight);
+                fscanf(fp,"%d",&p[count].price);
+
+                fscanf(fp,"%c",&trash);
+        }
+
+        fclose(fp);
 
 
 	printf("=> 로딩 성공!\n");
